2958e88e73aca573c4570fa71321e77ca626110e
gaoshiyu <gaoshiyu@intern.douban.com>
2017-05-07 00:29:28 +0800

    delete redundant class of EC

--- code/consts.py
+++ code/consts.py
@@ -2,8 +2,6 @@
 
 Exp_ecs = ['EXP', 'IDA', 'IPI', 'IMP', 'IGI', 'IEP']
 Cmp_ecs = ['ISS', 'ISO', 'ISA', 'ISM', 'IGC', 'IBA', 'IBD', 'IKR', 'IRD', 'RCA']
-As_ecs = ['TAS']
-Cs_ecs = ['IC']
 
 Nr_ecs = ['IEA']
 Unrl_ecs = ['NAS', 'ND']  # non-tracable author statement & no biological data available
